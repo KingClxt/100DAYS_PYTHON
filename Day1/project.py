b8d864609e530b9bf3a13f997327a438735f7634
KingClxt <hassanetinde@gmail.com>
2024-12-18 18:46:23 +0000

    first commit

---
+++ Day1/project.py
@@ -0,0 +1,7 @@
+print("Bienvenue sur le générateur de nom de groupe")
+
+cityName = input("Quel est le nom de la ville dans laquelle tu as grandi ?\n")
+
+animalName = input("Quel est le nom de votre animal compagnie ?\n")
+
+print("Le nom de votre groupe pourrais etre "+ cityName+'_'+animalName)

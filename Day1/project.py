a11459a77424ca5064d718261b2a5837617c10f0
KingClxt <hassanetinde@gmail.com>
2024-12-18 18:52:32 +0000

    second commit

--- Day1/project.py
+++ Day1/project.py
@@ -5,3 +5,4 @@ cityName = input("Quel est le nom de la ville dans laquelle tu as grandi ?\n")
 animalName = input("Quel est le nom de votre animal compagnie ?\n")
 
 print("Le nom de votre groupe pourrais etre "+ cityName+'_'+animalName)
+
